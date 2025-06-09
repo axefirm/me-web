f657caa8b463de639f80615a16e864c2e9a22104
Sukhbat <sukhbat@fiba.mn>
2025-06-09 17:59:05 +0800

    static pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/me.app.web",
-  output: "export",  // <=== enables static exports
+  // output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
 
